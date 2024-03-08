19013f25f47fb5711fa033273a3b32f7318fdd12
John Samuel <johnayobami77@proton.me>
2024-03-08 05:08:39 +0100

    Update views.py to use get_object_or_404

--- InfinityFinance/views.py
+++ InfinityFinance/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.views import LoginView, LogoutView, RegisterView
 from django.urls import reverse_lazy
 
