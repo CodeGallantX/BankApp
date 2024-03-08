02cfc64fae82dec0b41118fb14954b50a6522cfc
John Samuel <johnayobami77@proton.me>
2024-03-08 04:55:54 +0100

    modified models.py

--- InfinityFinance/models.py
+++ InfinityFinance/models.py
@@ -1,1 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class CustomUser(AbstractUser):
+    account_type = models.CharField(max_length=50)
+    balance = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+
+    class Meta:
+        # Specify unique related names for the groups and user_permissions fields
+        # to avoid clashes with the built-in auth.User model
+        permissions_related_name = 'custom_user_permissions'
+        groups_related_name = 'custom_user_groups'
+
+class Account(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    account_number = models.CharField(max_length=20, unique=True)
+    account_type = models.CharField(max_length=50)
+    balance = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+
+class Transaction(models.Model):
+    sender = models.ForeignKey(Account, on_delete=models.CASCADE, related_name='sender_transactions')
+    receiver = models.ForeignKey(Account, on_delete=models.CASCADE, related_name='receiver_transactions')
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    timestamp = models.DateTimeField(auto_now_add=True)
