49991d77447dd0996a8f08eaf98bcbcec64e40bb
John Samuel <johnayobami77@proton.me>
2024-03-08 05:36:50 +0100

    Add account details page

--- InfinityFinance/views.py
+++ InfinityFinance/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, get_object_or_404
-from django.contrib.auth.views import LoginView, LogoutView, RegisterView
+from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.forms import UserCreationForm
+from django.views import generic
 from django.urls import reverse_lazy
 from .models import Account
 
@@ -18,6 +20,7 @@ class CustomLoginView(LoginView):
 class CustomLogoutView(LogoutView):
     next_page = reverse_lazy('login')
 
-class CustomRegisterView(RegisterView):
-    template_name = 'bank/register.html'
+class RegisterView(generic.CreateView):
+    form_class = UserCreationForm
     success_url = reverse_lazy('login')
+    template_name = 'registration/register.html'
