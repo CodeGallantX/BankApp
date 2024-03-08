fefebfd65591bf009ba1d4d0a73bba60900a9c68
John Samuel <johnayobami77@proton.me>
2024-03-08 05:46:07 +0100

    Update import path for views.

--- BankApp/urls.py
+++ BankApp/urls.py
@@ -33,7 +33,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls import handler404
-from InfinityFinance import views
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
