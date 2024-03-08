d5510623a3650f890411fad79814e6f96ecbe8a1
John Samuel <johnayobami77@proton.me>
2024-03-08 05:01:50 +0100

    Update user and account model

--- InfinityFinance/models.py
+++ InfinityFinance/models.py
@@ -5,14 +5,8 @@ class CustomUser(AbstractUser):
     account_type = models.CharField(max_length=50)
     balance = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
 
-    class Meta:
-        # Specify unique related names for the groups and user_permissions fields
-        # to avoid clashes with the built-in auth.User model
-        permissions_related_name = 'custom_user_permissions'
-        groups_related_name = 'custom_user_groups'
-
 class Account(models.Model):
-    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='accounts')
     account_number = models.CharField(max_length=20, unique=True)
     account_type = models.CharField(max_length=50)
     balance = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
