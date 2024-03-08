9cb7c0d75dc46a45bdefcf87dc7796a3b45ad833
John Samuel <johnayobami77@proton.me>
2024-03-08 06:18:43 +0100

    Changed import paths to use dot notation instead of strings.

--- BankApp/urls.py
+++ BankApp/urls.py
@@ -7,13 +7,13 @@ app_name = 'bank'
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', 'InfinityFinance.views.home_page, name='home'),
-    path('account/', 'InfinityFinance.views.account', name='account'),
-    path('transfer/', 'InfinityFinance.views.transfer', name='transfer'),
-    path('deposit/', 'InfinityFinance.views.deposit', name='deposit'),
-    path('withdraw/', 'InfinityFinance.views.withdraw', name='withdraw'),
-    path('register/', 'InfinityFinance.views.signup', name="signup"),
-    path('account/<int:account_id>/', 'InfinityFinance.views.account_details', name="account_details"),
+    path('', InfinityFinance.views.home_page, name='home'),
+    path('account/', InfinityFinance.views.account, name='account'),
+    path('transfer/', InfinityFinance.views.transfer, name='transfer'),
+    path('deposit/', InfinityFinance.views.deposit, name='deposit'),
+    path('withdraw/', InfinityFinance.views.withdraw, name='withdraw'),
+    path('register/', InfinityFinance.views.signup, name="signup"),
+    path('account/<int:account_id>/', InfinityFinance.views.account_details, name="account_details"),
 ]
 
 handler404 = 'InfinityFinance.views.page_not_found'
