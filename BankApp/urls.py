3e661a51ed2e2d726240179b2306d9ceb62bc919
John Samuel <johnayobami77@proton.me>
2024-03-09 03:58:08 +0100

    Replace finance_views with InfinityFinance.views

--- BankApp/urls.py
+++ BankApp/urls.py
@@ -7,12 +7,12 @@ app_name = 'bank'
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home_page, name='home'),
-    path('account/', finance_views.account, name='account'),
-    path('transfer/', finance_views.transfer, name='transfer'),
-    path('deposit/', finance_views.deposit, name='deposit'),
-    path('withdraw/', finance_views.withdraw, name='withdraw'),
-    path('register/', finance_views.signup, name="signup"),
-    path('account/<int:account_id>/', finance_views.account_details, name="account_details"),
+    path('account/', InfinityFinance.views.account, name='account'),
+    path('transfer/', InfinityFinance.views.transfer, name='transfer'),
+    path('deposit/', InfinityFinance.views.deposit, name='deposit'),
+    path('withdraw/', InfinityFinance.views.withdraw, name='withdraw'),
+    path('register/', InfinityFinance.views.signup, name="signup"),
+    path('account/<int:account_id>/', InfinityFinance.views.account_details, name="account_details"),
 ]
 
 
