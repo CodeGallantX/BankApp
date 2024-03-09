8c3ca6d661c949f0920a8c4ed2861be862e8d303
John Samuel <johnayobami77@proton.me>
2024-03-09 07:28:09 +0100

    .

--- BankApp/settings.py
+++ BankApp/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-y*cmw%kx##*hv-pv#pz2n@r-o)(#$=gkt$cg7j=6duc%syiv6b
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -117,11 +117,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static")
+    BASE_DIR/ "InfinityFinance" / "static"
 ]
-STATIC_ROOT = os.path.join('BASE_DIR', 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
