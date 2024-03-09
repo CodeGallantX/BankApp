251810ba9c3523eb61f885a30c83de036432fa21
John Samuel <johnayobami77@proton.me>
2024-03-09 09:45:28 +0100

    Changed admin site name and title to 'Custom Admin Site'.

--- InfinityFinance/admin.py
+++ InfinityFinance/admin.py
@@ -4,3 +4,10 @@ from .models import Account, Transaction
 # Register your models here.
 admin.site.register(Account)
 admin.site.register(Transaction)
+
+# Creating Admin Site
+class CustomAdminSite(admin.AdminSite):
+    site_header = 'Custom Admin Site'
+    site_title = 'Custom Admin Site'
+
+custom_admin_site = CustomAdminSite(name='custom_admin')
