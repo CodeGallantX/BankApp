71851d603f66c7f9419d3a528ad2b9e132f4336a
John Samuel <johnayobami77@proton.me>
2024-03-09 09:49:31 +0100

    Move admin registration to custom admin site

--- InfinityFinance/admin.py
+++ InfinityFinance/admin.py
@@ -2,12 +2,16 @@ from django.contrib import admin
 from .models import Account, Transaction
 
 # Register your models here.
-admin.site.register(Account)
-admin.site.register(Transaction)
+
 
 # Creating Admin Site
 class CustomAdminSite(admin.AdminSite):
     site_header = 'Custom Admin Site'
     site_title = 'Custom Admin Site'
 
-custom_admin_site = CustomAdminSite(name='custom_admin')
+IF_admin_site = CustomAdminSite(name='IF_admin')
+
+# Register your models with the custom admin site
+IF_admin_site.register(Account)
+IF_admin_site.register(Transaction)
+
